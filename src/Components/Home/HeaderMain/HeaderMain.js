5806fff26ca73cfd06104083bea367b0b0ed43a8
samssjubair <samssjubair@gmail.com>
2021-04-18 16:29:22 +0600

    header responsive done

--- src/Components/Home/HeaderMain/HeaderMain.js
+++ src/Components/Home/HeaderMain/HeaderMain.js
@@ -3,7 +3,7 @@ import './HeaderMain.css';
 
 const HeaderMain = () => {
     return (
-        <div style={{zIndex: '3',textAlign: 'center'}} className="header-style d-flex t text-white">
+        <div style={{zIndex: '3',textAlign: 'center'}} className="header-style d-flex ps-0 text-white container-fluid">
             <div  className="mx-auto align-self-center header">
                 <h1  className="brand-gradient">EVERYTHING IS POSSIBLE!</h1>
                 <p >When you trust on Event possible, everything is Possible. Our goal is to make your event memorable forever</p>
